818732c9a1e0c59e054db32d635c0749fb70419a
Lukas Bombach <lukas.bombach@googlemail.com>
2019-01-29 09:51:59 +0100

    sketch + stuff

--- src/models/sevenZip/handlers/subStreamsInfo.js
+++ src/models/sevenZip/handlers/subStreamsInfo.js
@@ -1,4 +1,21 @@
-module.exports = async function subStreamsInfo(file) {
-  const allDefined = await file.int();
-  return ["subStreamsInfo"];
+const kNumUnPackStream = 0x0d;
+const kSize = 0x09;
+const kCRC = 0x0a;
+const kEnd = 0x00;
+
+const handlers = {
+  [kNumUnPackStream.toString()]: numUnPackStream,
+  [kSize.toString()]: size,
+  [kCRC.toString()]: crc
 };
+
+module.exports = async function subStreamsInfo(file) {
+  const firstByte = await file.byte();
+  const handler = handlers[firstByte.toString()];
+  if (firstByte === kEnd) return;
+  if (!handler) return [`No handler for subStreamsInfo ${firstByte.toString(16)}`];
+  const value = await handler(size);
+  return [...value, ...(await subStreamsInfo(file))];
+};
+
+async function numUnPackStream(file) {}
--- start.js
+++ start.js
@@ -1,6 +1,7 @@
 const Headers = require("./src/models/sevenZip/headers");
 
 (async () => {
+  console.log("\x1Bc");
   const path = `${__dirname}/src/models/sevenZip/7ziptest/btchamp.7z`;
   const headers = await Headers.get(path);
 
