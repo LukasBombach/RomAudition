9f12913d32c48f6b2b98658e0f2eb66523df2502
Lukas Bombach <lukas.bombach@googlemail.com>
2019-02-22 23:47:58 +0100

    working script indexing games

--- src/models/store.js
+++ src/models/store.js
@@ -8,18 +8,18 @@ class Store {
     const db = await Store.connectToDatabase();
     const collections = await db.collections();
     const collectionName = `${namespace}.${name}`;
-    if (!collections.includes(collectionName))
-      await db.createCollection(collectionName, options);
+    if (!collections.includes(collectionName)) await db.createCollection(collectionName, options);
     return await db.collection(collectionName);
   }
 
   static async connectToDatabase() {
     const options = { useNewUrlParser: true };
-    const client = await MongoClient.connect(
-      mongoUrl,
-      options
-    );
-    return await client.db(dbName);
+    Store.client = await MongoClient.connect(mongoUrl, options);
+    return await Store.client.db(dbName);
+  }
+
+  static async disconnect() {
+    await Store.client.close();
   }
 }
 
--- start.js
+++ start.js
@@ -2,21 +2,26 @@ const readDirRec = require("recursive-readdir");
 const lsar = require("lsar-native");
 const pretty = require("pretty-hrtime");
 const chalk = require("chalk");
+const Store = require("./src/models/store");
 
 async function indexFolder(path) {
+  const collection = await Store.getCollection("files", "files");
   const { baseDir, paths, numGames, time } = await listGames(path);
   const logInterval = Math.floor(numGames / 100);
   const hrstart = process.hrtime();
   console.log(chalk.dim(pretty(time)), `Found ${numGames} games`);
   for (let i = 0; i < numGames; i++) {
-    lsar(`${baseDir}/${paths[i]}`);
+    const game = getGame(baseDir, paths[i]);
+    const filter = { baseDir, path: game.path };
+    await collection.updateOne(filter, { $set: game }, { upsert: true });
     if (i % logInterval === 0) {
       const time = process.hrtime(hrstart);
       const percent = ((i / numGames) * 100).toFixed(2);
       console.log(chalk.dim(pretty(time)), `${percent}%`, `Game ${i} of ${numGames}`);
     }
   }
-  console.log(`Actually done in ${pretty(time)}`);
+  await Store.disconnect();
+  console.log(`✨  Actually done in ${pretty(time)} (${numGames} games)`);
 }
 
 async function listGames(baseDir) {
@@ -26,6 +31,17 @@ async function listGames(baseDir) {
   const numGames = paths.length;
   const time = process.hrtime(hrstart);
   return { baseDir, paths, numGames, time };
+}
+
+function getGame(baseDir, path) {
+  const info = lsar(`${baseDir}/${path}`);
+  const roms = info.map(({ XADFileName: name, "7zCRC32": crcInt, XADFileSize: size }) => ({ name, crc: crcInt.toString(16), size }));
+  const name = path.substring(path.lastIndexOf("/") + 1);
+  const crcSignature = roms
+    .map(({ crc }) => crc)
+    .sort()
+    .join(",");
+  return { name, baseDir, path, crcSignature, roms };
 }
 
 (async () => {
